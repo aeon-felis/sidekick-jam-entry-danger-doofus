19235542bc7578b4eee3e9d480dd72ff5b349367
IdanArye <idanarye@gmail.com>
2022-05-12 00:00:27 +0300

    Finish implementing camera auto-adjusting

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,6 @@
 use bevy::prelude::*;
-use bevy::render::camera::RenderTarget;
 use bevy_rapier2d::prelude::RigidBody;
-use bevy_yoleck::{YoleckManaged, YoleckEditorState};
+use bevy_yoleck::{YoleckEditorState, YoleckManaged};
 
 use crate::utils::some_or;
 
@@ -12,33 +11,17 @@ pub struct CameraPlugin {
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup_camera);
-        if false {
-            app.add_startup_system(update_camera_scale_to_window);
-            app.add_system_set(SystemSet::on_update(YoleckEditorState::GameActive).with_system(update_camera_transform));
-        } else {
-            app.add_system_set(SystemSet::new().with_system(update_camera_transform));
-        }
+        app.add_system_set(
+            SystemSet::on_update(YoleckEditorState::GameActive)
+                .with_system(update_camera_transform),
+        );
     }
 }
 
 fn setup_camera(mut commands: Commands) {
-    let camera = OrthographicCameraBundle::new_2d();
-    // camera.transform.translation.y = 0.0;
-    // camera.transform.translation.z = 0.0;
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.transform.scale = Vec3::new(0.016, 0.016, 1.0);
     commands.spawn_bundle(camera);
-}
-
-fn update_camera_scale_to_window(
-    mut query: Query<(&Camera, &mut OrthographicProjection)>,
-    windows: Res<Windows>,
-) {
-    for (camera, mut projection) in query.iter_mut() {
-        if let RenderTarget::Window(window_id) = camera.target {
-            if let Some(window) = windows.get(window_id) {
-                projection.scale = 15.0 / window.width();
-            }
-        }
-    }
 }
 
 fn update_camera_transform(
@@ -47,7 +30,7 @@ fn update_camera_transform(
 ) {
     let mut minmax: Option<[f32; 4]> = None;
     for (global_transform, sprite, rigid_body) in non_dynamic_objects_query.iter() {
-        if *rigid_body == RigidBody::Dynamic {
+        if *rigid_body != RigidBody::Fixed {
             continue;
         }
         let half_size = 0.5 * sprite.custom_size.unwrap().extend(0.0);
@@ -65,16 +48,16 @@ fn update_camera_transform(
         };
     }
     let minmax = some_or!(minmax; return);
-    // let objects_botleft = Vec3::new(minmax[0], minmax[1], 0.0);
-    // let objects_topright = Vec3::new(minmax[2], minmax[3], 0.0);
-    let world_widgth = minmax[2] - minmax[0];
+    let world_width = minmax[2] - minmax[0];
+    let world_height = minmax[3] - minmax[1];
     for (_camera, mut transform, projection) in cameras_query.iter_mut() {
         let projection_width = projection.right - projection.left;
-        transform.scale.x = world_widgth / projection_width;
-        transform.scale.y = transform.scale.x;
-        // info!("{} {}", projection_width, world_widgth);
-        // let camera_botleft = Vec3::new(projection.left, projection.bottom, 0.0);
-        // let camera_topright = Vec3::new(projection.right, projection.top, 0.0);
-        // info!("{:?} {:?} {:?}", objects_topright, transform.mul_vec3(camera_topright), projection);
+        let projection_height = projection.top - projection.bottom;
+        let width_ratio = world_width / projection_width;
+        let height_ratio = world_height / projection_height;
+        let chosen_ratio = width_ratio.max(height_ratio) * 1.1;
+        transform.scale = Vec3::new(chosen_ratio, chosen_ratio, 1.0);
+        transform.translation.x = 0.5 * (minmax[0] + minmax[2]);
+        transform.translation.y = 0.5 * (minmax[1] + minmax[3]);
     }
 }
