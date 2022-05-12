77403c7e0a3f5e32953d911f5f9090a60e18219a
IdanArye <idanarye@gmail.com>
2022-05-13 00:51:20 +0300

    Fix rebuild on asset change problem

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ extern crate embed_resource;
 use std::env;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build/windows/icon.ico");
     let target = env::var("TARGET").unwrap();
     if target.contains("windows") {
         // on windows we will set our game icon as icon for the executable
